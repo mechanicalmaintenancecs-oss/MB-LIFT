04c39cd398f87ca8acedebe1e3b589c66163c5c3
mechanicalmaintenancecs-oss <mechanical.maintenance.cs@gmail.com>
2026-08-03 16:56:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 
+document.addEventListener("DOMContentLoaded", () => {
+
+    // Hide Loader
+    const loader = document.getElementById("loader");
+    if(loader){
+        setTimeout(()=>{
+            loader.style.display = "none";
+        },500);
+    }
+
+});
